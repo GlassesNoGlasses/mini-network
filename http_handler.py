53a6947b92d5fd364109cdbe83abdc317c5e8c82
Neil Wang <neilywang3@gmail.com>
2025-02-21 16:32:55 -0500

    fixed duplicate bug fetching multi files

--- http_handler.py
+++ http_handler.py
@@ -11,16 +11,23 @@ The class is used in the BaseServer class in server.py.
 '''
 
 import os, fnmatch
+from zipfile import ZipFile
 from http.server import BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
 
 class HTTPRequestHandler(BaseHTTPRequestHandler):
 
-    def find_files(self, files, duplicates: bool = False) -> dict[str, list[str]]:
+    def find_files(self, files: list[str], duplicates: bool = False) -> dict[str, list[str]]:
         ''' Finds specified files "files" in the server directory. 
             @param files: list[str] - The files to find.
-            @param duplicates: bool - (optional) Whether to allow duplicate file names in the response.
+            @param duplicates: bool - (optional) Whether to allow duplicate file names in the response. 
+            Returns last found file if duplicates is False, else returns all found files.
         '''
+        print(f"Finding files: {files}")
+        print(f"Allow duplicates: {duplicates}")
+
+        if not files:
+            return 
 
         root_dir = self.server._root
         found_files = {file_name.lower(): [] for file_name in files}
@@ -28,7 +35,7 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
         for root, _, dir_files in os.walk(root_dir):
             for file in dir_files:
                 if file.lower() in found_files:
-                    if not duplicates and not found_files[file]:
+                    if not duplicates:
                         found_files[file] = [os.path.join(root, file)]
                     else:
                         found_files[file].append(os.path.join(root, file))
@@ -42,9 +49,12 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
         print(f"Headers: {headers}")
         print(f"URL: {url}")
         print(f"Query components: {query_components}")
-
+ 
         requested_files = query_components.get("files", [])
         allow_duplicates = query_components.get("allow_duplicates", False)
+
+        if allow_duplicates: 
+            allow_duplicates = allow_duplicates[0].lower() == "true"
 
         found_files = self.find_files(requested_files, allow_duplicates)
         print(f"Found files: {found_files}")
@@ -53,17 +63,27 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_response(404)
             self.end_headers()
             return
-
+        
         # TODO: modify headers and response based on found files
         self.send_response(200)
         self.send_header('Content-type','text/html') 
         self.end_headers()
-
-        for file, locations in found_files.items():
-            for location in locations:
-                with open(location, 'rb') as f:
-                    self.wfile.write(f.read())
-
+        
+        if len(found_files) == 1:
+            file_name = list(found_files.keys())[0]
+            file_location = found_files[file_name][0]
+            with open(file_location, 'rb') as f:
+                self.wfile.write(f.read())
+        else:
+            # Create a zip file containing the found files
+            with ZipFile('GET_files.zip', 'a') as zipf:
+                for locations in found_files.values():
+                    for file_path in locations:
+                        zipf.write(file_path)
+            # Send the zip file
+            with open('GET_files.zip', 'rb') as f:
+                self.wfile.write(f.read())
+            
 
     def do_POST(self):
         self.send_response(200)
